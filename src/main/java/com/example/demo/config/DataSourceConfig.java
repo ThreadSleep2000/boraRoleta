7ec525730f601a3ac049c6ec3123f650d21e978e
Matheus Martins <126476964+ThreadSleep2000@users.noreply.github.com>
2025-11-02 16:10:13 -0300

    feat: separação de ambientes para teste

---
+++ src/main/java/com/example/demo/config/DataSourceConfig.java
@@ -0,0 +1,29 @@
+package com.example.demo.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import javax.sql.DataSource;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    @Profile("prod")
+    public DataSource prodDataSource(DataSourceProperties props) {
+        return props.initializeDataSourceBuilder().type(HikariDataSource.class).build();
+    }
+
+    @Bean
+    @Profile("test")
+    public DataSource testDataSource() {
+        HikariDataSource ds = new HikariDataSource();
+        ds.setJdbcUrl("jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
+        ds.setUsername("sa");
+        ds.setPassword("");
+        ds.setDriverClassName("org.h2.Driver");
+        return ds;
+    }
+}
